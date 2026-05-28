408e150577765bd59ef747d0b2a0a68690c4626c
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2026-05-27 17:59:37 -0700

    update satty config

--- applications/satty.nix
+++ applications/satty.nix
@@ -8,6 +8,10 @@
         early-exit = true;
         initial-tool = "brush";
         brush-smooth-history-size = 5;
+        actions-on-right-click = [
+            "save-to-clipboard"
+            "exit"
+        ];
       };
 
       color-palette = {
